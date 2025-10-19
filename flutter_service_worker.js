a07ff0c0032a74bda8daf10c9f1da23e302fb0b4
Janosch <11590197+janoschp@users.noreply.github.com>
2025-10-19 15:25:26 +0200

    status bar fix

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,10 +3,10 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "43cd41eebc9a5b30ff8490b64b3b966c",
+const RESOURCES = {"flutter_bootstrap.js": "b6f00fb231ef840abb029d4b7fcd4471",
 "version.json": "b2f4a213a4aa2cc8d91d022fb2357fca",
-"index.html": "fd765378a5fd6a45dbfbb0e28b4a1f4c",
-"/": "fd765378a5fd6a45dbfbb0e28b4a1f4c",
+"index.html": "1565a55bfe49f35b3823dfa2d7ca289e",
+"/": "1565a55bfe49f35b3823dfa2d7ca289e",
 "main.dart.js": "aabd69946d13b79379eb2440ce89f655",
 "flutter.js": "888483df48293866f9f41d3d9274a779",
 "favicon.png": "c4285c3c087a7b934243698e0babc508",
@@ -14,7 +14,7 @@ const RESOURCES = {"flutter_bootstrap.js": "43cd41eebc9a5b30ff8490b64b3b966c",
 "icons/Icon-maskable-192.png": "761bc355e387c0ee8fe4846f744ce69a",
 "icons/Icon-maskable-512.png": "b35377f9c3da5dcd45a3273b2af989e0",
 "icons/Icon-512.png": "b35377f9c3da5dcd45a3273b2af989e0",
-"manifest.json": "68750b251d8c59f27c14d8ddb0002ac9",
+"manifest.json": "bfad2b63b36452e977e587aee0987b2a",
 "assets/AssetManifest.json": "99914b932bd37a50b983c5e7c90ae93b",
 "assets/NOTICES": "1e72984f4413327127fc4c84b51861b8",
 "assets/FontManifest.json": "d751713988987e9331980363e24189ce",
